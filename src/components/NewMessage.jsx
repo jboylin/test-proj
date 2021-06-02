a015d811a596966b7b1ff415f3b6a3dd29372807
Robert Wearden <robertwearden2@gmail.com>
2021-06-02 11:51:27 +0100

    add basic structure of component

--- src/components/NewMessage.jsx
+++ src/components/NewMessage.jsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
-const NewMessage = () => {
-  return <div>{/* DO SOME SODE HERE */}</div>;
+const NewMessage = (message) => {
+  // Message comes from PostMessageForm component (event.target.value)
+  //   const [message, setMessage] = useState('');
+  const { user } = useContext(UserContext);
+
+  return (
+    <div>
+      <p>{user}</p>
+      <p>{new Date()}</p>
+      <p>{message}</p>
+    </div>
+  );
 };
 
 export default NewMessage;
